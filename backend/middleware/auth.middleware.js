358e877373a4680249c6b8df6a1d9058327fd1ca
Himanshu Kumar Verma <76449638+Himanshuiiit@users.noreply.github.com>
2025-07-06 12:09:54 +0530

    Update auth.middleware.js

--- backend/middleware/auth.middleware.js
+++ backend/middleware/auth.middleware.js
@@ -1,6 +1,5 @@
 import jwt from "jsonwebtoken";
 import userModel from '../models/user.model.js';
-import redisClient from "../services/redis.service.js";
 
 
 export const authUser = async (req, res, next) => {
@@ -10,17 +9,7 @@ export const authUser = async (req, res, next) => {
         if (!token) {
             return res.status(401).send({ error: 'Unauthorized User' });
         }
-
-        const isBlackListed = await redisClient.get(token);
-
-        if (isBlackListed) {
-
-            res.cookie('token', '');
-
-            return res.status(401).send({ error: 'Unauthorized User' });
-        }
        
-
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         req.user = decoded;
         next();
