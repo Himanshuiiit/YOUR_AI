4d88a3ffddd1edc4d16af449169192d5de3ed455
Himanshu Kumar Verma <76449638+Himanshuiiit@users.noreply.github.com>
2025-07-06 11:27:30 +0530

    Update redis.service.js

--- backend/services/redis.service.js
+++ backend/services/redis.service.js
@@ -1,9 +1,7 @@
 import Redis from 'ioredis';
 
 // Initialize Redis using the connection URL
-const redisClient = new Redis("rediss://default:Eie94OGS1K21t0eFdmB00jzU6gtc4N2q@redis-15461.c305.ap-south-1-1.ec2.redns.redis-cloud.com:15461", {
-    tls: {}
-});
+const redisClient = new Redis("rediss://default:Eie94OGS1K21t0eFdmB00jzU6gtc4N2q@redis-15461.c305.ap-south-1-1.ec2.redns.redis-cloud.com:15461");
 
 redisClient.on('connect', () => {
     console.log('Redis connected');
